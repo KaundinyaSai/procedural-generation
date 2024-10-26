70f53ec9350fa3b9e46cd5ca82be3f197f7fe990
kaundinya <kaundinya.kautilya@gmail.com>
2024-10-26 12:41:57 +0530

    add colors based on hieght and seeds for random genration

--- Assets/scripts/HeightMapGenrater.cs
+++ Assets/scripts/HeightMapGenrater.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HeightMapGenrater 
 {
-    public static float GenrateHeightMap(float xScale, float yScale, int numOfOctaves, float persistance, float lacunarity) {
+    public static float GenrateHeightMap(float xScale, float yScale, int numOfOctaves, float persistance, float lacunarity, int seed) {
         float totalNoise = 0;
         float maxAmplitude = 0;
 
@@ -10,8 +10,11 @@ public class HeightMapGenrater
             float frequency = Mathf.Pow(lacunarity, i); // lacunarity is increase in frequency with each octave
             float amplitude = Mathf.Pow(persistance, i); // pesistence is decrease in amplitude with each octave
 
+            float xNoise = xScale * frequency + seed * 0.1f;
+            float yNoise = yScale * frequency + seed * 0.1f;
+
             
-            totalNoise += Mathf.PerlinNoise(xScale * frequency, yScale * frequency) * amplitude;
+            totalNoise += Mathf.PerlinNoise(xNoise, yNoise) * amplitude;
             maxAmplitude += amplitude;
         }
         return totalNoise / maxAmplitude; // normalize to range 0 to 1
--- Assets/scripts/TerrainGenrater.cs
+++ Assets/scripts/TerrainGenrater.cs
@@ -11,44 +11,73 @@ public class TerrainGenrater : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    Color[] colors;
+    public Gradient gradient;
+
+    float minHeight;
+    float maxHeight;
+
+    public int octaves;
+    public float persistance;
+    public float lacunarity;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        vertices = CreateVertices(width, height);
-        triangles = CreateTriangles(width, height);
-        CreateGroundMesh(vertices, triangles);
+        CreateGroundMesh();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            CreateGroundMesh();
+        }
     }
 
-    void CreateGroundMesh(Vector3[] vertices, int[] triangles){
+    void CreateGroundMesh() {
+        vertices = CreateVertices(width, height);
+        triangles = CreateTriangles(width, height);
+        colors = CreateColorss(width, height);
+        
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors = colors;
 
         mesh.RecalculateNormals(); // for lighting
         mesh.RecalculateBounds(); // to make sure bounds are accurate
     }
-   Vector3[] CreateVertices(int width, int height) {
+    Vector3[] CreateVertices(int width, int height) {
         vertices = new Vector3[(width + 1) * (height + 1)];
 
-        for(int index = 0, z = 0; z <= height; z++) {
-            for(int x = 0; x <= width; x++) {
-                float y = HeightMapGenrater.GenrateHeightMap(x * scale, z * scale, 6, 0.5f, 2.0f);
-                
-                vertices[index] = new Vector3(x , y, z);
+        // Random offsets for variation
+        int seed = Random.Range(0, 1000000);
+        
+        for (int index = 0, z = 0; z <= height; z++) {
+            for (int x = 0; x <= width; x++) {
+                // Using random offsets in the height generation
+                float y = HeightMapGenrater.GenrateHeightMap(
+                    (float)(x * scale ), 
+                    (float)(z * scale ), 
+                    octaves, 
+                    persistance, 
+                    lacunarity,
+                    seed
+                ) * heightMultiplier;
+
+                vertices[index] = new Vector3(x, y, z);
+                maxHeight = Mathf.Max(maxHeight, vertices[index].y);
+                minHeight = Mathf.Min(minHeight, vertices[index].y);
                 index++;
             }
         }
         return vertices;
     }
+
 
     int[] CreateTriangles(int width, int height) {
         triangles = new int[width * height * 6];
@@ -71,6 +100,20 @@ public class TerrainGenrater : MonoBehaviour
             vertex++;
         }
         return triangles;
+    }
+
+    Color[] CreateColorss(int width, int height) {
+        colors = new Color[vertices.Length];
+        for(int index = 0, z = 0; z <= height; z++) {
+            for(int x = 0; x <= width; x++) {
+                float vertexHeight = vertices[index].y;
+                
+                float normalizedHeight = Mathf.InverseLerp(minHeight, maxHeight, vertexHeight);
+                colors[index] = gradient.Evaluate(normalizedHeight);
+                index++;
+            }
+        }
+        return colors;
     }
 
     void OnDrawGizmosSelected(){
